9f080fec43b22e2738acb1c5f55e4731db2b8b00
Donghwi Kim <dhkim09a@gmail.com>
2023-10-19 15:46:06 +0900

    fix error when executing a subcommand programatically

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="subcmdparse", # Replace with your own username
-    version="0.1.7",
+    version="0.1.8",
     author="Donghwi Kim",
     author_email="dhkim09@kaist.ac.kr",
     description="Subcommand extension for argparse package",
--- src/subcmdparse/util.py
+++ src/subcmdparse/util.py
@@ -6,6 +6,6 @@ import sh
 def compile_shargs(*args, **kwargs) -> Tuple[list, dict]:
     shcmd = sh.Command('/bin/ls').bake(*args, **kwargs)
     return (
-        [a.decode('utf-8') for a in shcmd._partial_baked_args],
+        [a if isinstance(a, str) else a.decode('utf-8') for a in shcmd._partial_baked_args],
         {'_' + k: i for k, i in shcmd._partial_call_args.items()}
     )
